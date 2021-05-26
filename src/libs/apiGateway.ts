a97f43ab2325d1dc84fcf394e85e24398dd3149a
sieonov <sieonov545@gmail.com>
2021-05-26 05:08:56 +0300

    initial commit

---
+++ src/libs/apiGateway.ts
@@ -0,0 +1,35 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import type { FromSchema } from "json-schema-to-ts";
+import isEmpty from 'lodash/get';
+
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+
+interface IResponse {
+  response: Record<string, unknown> | string;
+  statusCode?: number;
+  headers?: Record<string, unknown>;
+}
+
+export const formatJSONResponse = ({ headers, statusCode, response }: IResponse) => {
+  headers = isEmpty(headers) ? {} : headers;
+
+  return {
+    statusCode: statusCode || 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      ...headers,
+    },
+    body: typeof response === 'string' ? response : JSON.stringify(response),
+  }
+}
+
+
+
+export const formatJSONResponse = (response: Record<string, unknown>) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response)
+  }
+}
